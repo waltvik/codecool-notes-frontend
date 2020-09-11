61be2a68ada9bc55b5ee4ee07c16d82dd893f0f3
sibaluca12 <sibaluca@gmail.com>
2020-09-11 19:04:40 +0200

    settings page ui done

--- src/pages/settings/SettingsPage.js
+++ src/pages/settings/SettingsPage.js
@@ -1,14 +1,37 @@
 import React from "react";
 import Menu from "../../components/Menu";
+import "./settings.css";
 
 const SettingsPage = () => {
+  const darkMode = localStorage.getItem("dark");
+
   return (
     <div>
       <div className="settings-container">
-        <div>Light/dark mode</div>
-        <div>User pic</div>
-        <div>User/password change</div>
-        <div>Email</div>
+        <div className="toggle-settings-container">
+          <span className="light-mode-settings">Light/dark mode</span>
+          <span className="toggle-container">
+            <span style={{ color: darkMode ? "grey" : "yellow" }}>☀︎</span>
+            <span className="toggle">
+              <input
+                checked={darkMode}
+                // onChange={() => setDarkMode((prevMode) => !prevMode)}
+                id="checkbox"
+                className="checkbox"
+                type="checkbox"
+              />
+              <label htmlFor="checkbox" />
+            </span>
+            <span style={{ color: darkMode ? "slateblue" : "grey" }}>☾</span>
+            {/* <button onClick={() => setDarkMode(prevMode => !prevMode)}>
+          Toggle
+        </button> */}
+          </span>
+        </div>
+
+        <div className="settings-item">User pic</div>
+        <div className="settings-item">User/password change</div>
+        <div className="settings-item">Email</div>
       </div>
       <Menu />
     </div>
