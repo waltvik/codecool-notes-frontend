2752e12559fece209ab6c531a8908d7cb424f1e0
Vivien Walter <walter.vivien.k@gmail.com>
2020-09-15 16:32:27 +0200

    theme-switcher without context

--- src/App.js
+++ src/App.js
@@ -1,44 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import WelcomePage from "./pages/welcome/WelcomePage";
 import RegistrationPage from "./pages/registration/RegistrationPage";
 import SettingsPage from "./pages/settings/SettingsPage";
 import MainPage from "./pages/main/MainPage";
-import ModuleBar from "./components/module/ModuleBar";
 import ModulePage from "./pages/module/ModulePage";
 
 function App() {
-  const [darkMode, setDarkMode] = React.useState(getInitialMode());
-  React.useEffect(() => {
-    localStorage.setItem("dark", JSON.stringify(darkMode));
-  }, [darkMode]);
+  const [darkMode, setDarkMode] = useState(
+    JSON.parse(localStorage.getItem("dark"))
+  );
 
-  function handleChange(newValue) {
-    console.log(newValue);
-    setDarkMode(newValue);
-  }
-
-  function getInitialMode() {
-    const isReturningUser = "dark" in localStorage;
-    const savedMode = JSON.parse(localStorage.getItem("dark"));
-    const userPrefersDark = getPrefColorScheme();
-    // if mode was saved --> dark / light
-    if (isReturningUser) {
-      return savedMode;
-      // if preferred color scheme is dark --> dark
-    } else if (userPrefersDark) {
-      return true;
-      // otherwise --> light
-    } else {
-      return false;
-    }
-    // return savedMode || false;
-  }
-
-  function getPrefColorScheme() {
-    if (!window.matchMedia) return;
-  }
+  useEffect(() => {
+    setDarkMode(JSON.parse(localStorage.getItem("dark")));
+  }, []);
 
   return (
     <Router>
@@ -54,13 +30,7 @@ function App() {
         <Route exact path="/jobhunt" component={ModulePage} />
         <Route exact path="/welcome" component={WelcomePage} />
         <Route exact path="/registration" component={RegistrationPage} />
-        <Route
-          exact
-          path="/settings"
-          component={SettingsPage}
-          // darkMode={darkMode}
-          // onChange={handleChange}
-        />
+        <Route exact path="/settings" component={SettingsPage} />
       </div>
     </Router>
   );
--- src/pages/settings/SettingsPage.js
+++ src/pages/settings/SettingsPage.js
@@ -4,25 +4,34 @@ import "./settings.css";
 import SettingsHeader from "./SettingsHeader";
 
 const SettingsPage = (props) => {
-  const darkMode = localStorage.getItem("dark");
-  const boolValue = darkMode === "true";
+  const [darkMode, setDarkMode] = React.useState(getInitialMode());
+  React.useEffect(() => {
+    localStorage.setItem("dark", JSON.stringify(darkMode));
+  }, [darkMode]);
 
-  //   function handleChange(event) {
-  //     // Here, we invoke the callback with the new value
-  //     if (props.value == true) {
-  //       props.onChange(false);
-  //     } else {
-  //       props.onChange(true);
-  //     }
-  //     // props.onChange(event.target.value);
-  //   }
+  function getInitialMode() {
+    const isReturningUser = "dark" in localStorage;
+    const savedMode = JSON.parse(localStorage.getItem("dark"));
+    const userPrefersDark = getPrefColorScheme();
 
-  //   function valami() {
-  //     props.mode == true ? props.mode == false : props.mode == true;
-  //   }
+    if (isReturningUser) {
+      return savedMode;
+    } else if (userPrefersDark) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function getPrefColorScheme() {
+    if (!window.matchMedia) return;
+  }
 
   return (
-    <div>
+    <div
+      style={{ height: "100%" }}
+      className={darkMode ? "dark-mode" : "light-mode"}
+    >
       <SettingsHeader urlToAvatar="https://www.pexels.com/hu-hu/foto/allat-aranyos-bajusz-belfoldi-1314550/" />
       <div className="settings-container">
         <div className="toggle-settings-container">
@@ -31,10 +40,8 @@ const SettingsPage = (props) => {
             <span style={{ color: darkMode ? "grey" : "yellow" }}>☀︎</span>
             <span className="toggle">
               <input
-                defaultChecked={darkMode}
-                // onChange={() => valami()}
-                // onChange={handleChange}
-                //  onChange={() => setDarkMode((prevMode) => !prevMode)}
+                checked={darkMode}
+                onChange={() => setDarkMode((prevMode) => !prevMode)}
                 id="checkbox"
                 className="checkbox"
                 type="checkbox"
@@ -42,9 +49,6 @@ const SettingsPage = (props) => {
               <label htmlFor="checkbox" />
             </span>
             <span style={{ color: darkMode ? "slateblue" : "grey" }}>☾</span>
-            {/* <button onClick={() => setDarkMode(prevMode => !prevMode)}>
-          Toggle
-        </button> */}
           </span>
         </div>
 
